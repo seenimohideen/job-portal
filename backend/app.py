fb49737d4a58df795271a5f5aad76fc775b3821b
seeni-mohideen <seenimohideen9673@gmail.com>
2026-05-06 19:23:26 +0530

    backend folder setup

--- backend/app.py
+++ backend/app.py
@@ -151,4 +151,6 @@ def get_applications(user_id):
     return jsonify(result)
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
